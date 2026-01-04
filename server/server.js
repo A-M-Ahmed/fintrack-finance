6e08427c0f49b90061a4d769f74d07d1ef2fc500
A-M-Ahmed <abdirahmannadaara42@gmail.com>
2026-01-04 03:40:52 +0300

    chore: remove admin dashboard feature

--- server/server.js
+++ server/server.js
@@ -52,14 +52,14 @@ app.use('/api/dashboard', dashboardRoutes);
 app.use('/api/invoices', invoiceRoutes);
 
 // Serve frontend in production
-if (process.env.NODE_ENV === 'production') { 
+if (process.env.NODE_ENV === 'production') {
 
-    const  __dirname = path.dirname(fileURLToPath(import.meta.url))
+    const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
     app.get(/.*/, (req, res) => {
-        res.sendFile(path.join(__dirname, '..','client','dist','index.html'));
+        res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
     });
 }
 
